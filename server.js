7845f261b5153ddc36a0b5239f842f36c336b951
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-11-25 17:38:56 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import dotenv from 'dotenv'
+import yaml from 'yamljs'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+// Load swagger documentation
+const swaggerDocument = yaml.load('./swagger/swagger.yaml')
+
+// Middleware to serve Swagger UI
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// Base route
+app.get('/', (req, res) => {
+    res.send("Welcome to FunApi! Go to /docs for API documentation")
+})
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`)
+})
